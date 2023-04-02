64d62dfdad3785dcedd62bdc126d00f48f3aacb2
Niccolò Toccane <nico.toccane@gmail.com>
2023-04-02 13:38:56 +0200

    update CV link

--- components/navbar.js
+++ components/navbar.js
@@ -1,118 +1,118 @@
 import Logo from './logo';
 import NextLink from 'next/link';
 import {
-  Container,
-  Box,
-  Link,
-  Stack,
-  Heading,
-  Flex,
-  Menu,
-  MenuItem,
-  MenuList,
-  MenuButton,
-  IconButton,
-  useColorModeValue,
+ Container,
+ Box,
+ Link,
+ Stack,
+ Heading,
+ Flex,
+ Menu,
+ MenuItem,
+ MenuList,
+ MenuButton,
+ IconButton,
+ useColorModeValue,
 } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import ThemeToggleButton from './theme-toggle-button';
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
-  const active = path === href;
-  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900');
-  return (
-    <NextLink href={href} passHref scroll={false}>
-      <Link
-        p={2}
-        bg={active ? 'orange.500' : undefined}
-        borderRadius="md"
-        color={active ? 'white' : inactiveColor}
-        target={target}
-        {...props}
-      >
-        {children}
-      </Link>
-    </NextLink>
-  );
+ const active = path === href;
+ const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900');
+ return (
+  <NextLink href={href} passHref scroll={false}>
+   <Link
+    p={2}
+    bg={active ? 'orange.500' : undefined}
+    borderRadius="md"
+    color={active ? 'white' : inactiveColor}
+    target={target}
+    {...props}
+   >
+    {children}
+   </Link>
+  </NextLink>
+ );
 };
 
 const Navbar = (props) => {
-  const { path } = props;
+ const { path } = props;
 
-  return (
-    <Box
-      position="fixed"
-      as="nav"
-      w="100%"
-      bg={useColorModeValue('#ffffff40', '#20202380')}
-      css={{ backdropFilter: 'blur(10px)' }}
-      zIndex={2}
-      {...props}
+ return (
+  <Box
+   position="fixed"
+   as="nav"
+   w="100%"
+   bg={useColorModeValue('#ffffff40', '#20202380')}
+   css={{ backdropFilter: 'blur(10px)' }}
+   zIndex={2}
+   {...props}
+  >
+   <Container
+    display="flex"
+    p={2}
+    maxW="container.md"
+    wrap="wrap"
+    align="center"
+    justify="space-between"
+   >
+    <Flex align="center" mr={5}>
+     <Heading as="h1" size="lg" letterSpacing={1}>
+      <Logo />
+     </Heading>
+    </Flex>
+
+    <Stack
+     direction={{ base: 'column', md: 'row' }}
+     display={{ base: 'none', md: 'flex' }}
+     width={{ base: 'full', md: 'auto' }}
+     alignItems="center"
+     flexGrow={1}
+     mt={{ base: 4, md: 0 }}
     >
-      <Container
-        display="flex"
-        p={2}
-        maxW="container.md"
-        wrap="wrap"
-        align="center"
-        justify="space-between"
-      >
-        <Flex align="center" mr={5}>
-          <Heading as="h1" size="lg" letterSpacing={1}>
-            <Logo />
-          </Heading>
-        </Flex>
+     <LinkItem href="/projects" path={path}>
+      Personal projects
+     </LinkItem>
+     <LinkItem href="https://res.cloudinary.com/dmxuerbxv/image/upload/v1680435295/portfolio/Niccolo%CC%80-Toccane-CV_myxv82.pdf">
+      CV
+     </LinkItem>
+     <LinkItem href="/contacts" path={path}>
+      Contact me
+     </LinkItem>
+    </Stack>
 
-        <Stack
-          direction={{ base: 'column', md: 'row' }}
-          display={{ base: 'none', md: 'flex' }}
-          width={{ base: 'full', md: 'auto' }}
-          alignItems="center"
-          flexGrow={1}
-          mt={{ base: 4, md: 0 }}
+    <Box flex={1} align="right">
+     <ThemeToggleButton />
+
+     <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+      <Menu isLazy id="navbar-menu">
+       <MenuButton
+        as={IconButton}
+        icon={<HamburgerIcon />}
+        variant="outline"
+        aria-label="Options"
+       />
+       <MenuList>
+        <NextLink href="/projects" passHref>
+         <MenuItem as={Link}>Personal projects</MenuItem>
+        </NextLink>
+        <NextLink
+         href="https://res.cloudinary.com/dmxuerbxv/image/upload/v1660811399/portfolio/Niccolo%CC%80-Toccane-CV_qgyxth.pdf"
+         passHref
         >
-          <LinkItem href="/projects" path={path}>
-            Personal projects
-          </LinkItem>
-          <LinkItem href="https://res.cloudinary.com/dmxuerbxv/image/upload/v1671542417/portfolio/Niccolo%CC%80-Toccane-CV_ufsqft.pdf">
-            CV
-          </LinkItem>
-          <LinkItem href="/contacts" path={path}>
-            Contact me
-          </LinkItem>
-        </Stack>
-
-        <Box flex={1} align="right">
-          <ThemeToggleButton />
-
-          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
-            <Menu isLazy id="navbar-menu">
-              <MenuButton
-                as={IconButton}
-                icon={<HamburgerIcon />}
-                variant="outline"
-                aria-label="Options"
-              />
-              <MenuList>
-                <NextLink href="/projects" passHref>
-                  <MenuItem as={Link}>Personal projects</MenuItem>
-                </NextLink>
-                <NextLink
-                  href="https://res.cloudinary.com/dmxuerbxv/image/upload/v1660811399/portfolio/Niccolo%CC%80-Toccane-CV_qgyxth.pdf"
-                  passHref
-                >
-                  <MenuItem as={Link}>CV</MenuItem>
-                </NextLink>
-                <NextLink href="/contacts" passHref>
-                  <MenuItem as={Link}>Contact me</MenuItem>
-                </NextLink>
-              </MenuList>
-            </Menu>
-          </Box>
-        </Box>
-      </Container>
+         <MenuItem as={Link}>CV</MenuItem>
+        </NextLink>
+        <NextLink href="/contacts" passHref>
+         <MenuItem as={Link}>Contact me</MenuItem>
+        </NextLink>
+       </MenuList>
+      </Menu>
+     </Box>
     </Box>
-  );
+   </Container>
+  </Box>
+ );
 };
 
 export default Navbar;
